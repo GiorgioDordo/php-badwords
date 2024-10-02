0535d3fe23536e39903f0ff2acd82837eb229211
GiorgioDordo <djole6789@gmail.com>
2024-10-02 16:41:06 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <main class="container" method="get">
+        <form action="badwords.php">
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Zumbo è una vrenzola</label>
+                <input type="input" class="form-control" id="exampleFormControlInput1" placeholder="Zumbo è una vrenzola">
+            </div>
+
+            <div class="mb-3">
+                <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+        </main>
+    </form>
+</body>
+
+</html>
